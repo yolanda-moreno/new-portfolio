d1accb6c5086591c7b46d41725c7a00c0f00a1b9
yolanda-moreno <yolimoreno98@gmail.com>
2024-01-18 19:48:48 +0100

    Home component completition

---
+++ yolanda-portfolio/src/components/home/HeaderSocials.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function HeaderSocials() {
+    return (
+        <div className="home__socials">
+            <a href="www.google.com" className="home__social-link" target="_blank">
+                <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="www.google.com" className="home__social-link" target="_blank">
+                <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="www.google.com" className="home__social-link" target="_blank">
+                <i className="fa-brands fa-codepen"></i>
+            </a>
+        </div>
+    )
+}
+
+export default HeaderSocials
